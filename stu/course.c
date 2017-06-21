3a45cb2eede67f9b1d2bcc3b043a43f3c3f9abbd
linux <hexuemiao@outlook.com>
2017-06-21 02:11:05 -0700

    Add teach function
    Function update with teacher

--- stu/course.c
+++ stu/course.c
@@ -18,7 +18,7 @@ int cgiMain()
 		</head>");
 
 	FILE * fd;
-	char sname[32] = "\0";
+	char cid[32] = "\0";
 	int status = 0;
 	char ch;
 
@@ -34,10 +34,10 @@ int cgiMain()
 	}
 	fclose(fd);
 
-	status = cgiFormString("sname",  sname, 32);
+	status = cgiFormString("cid",  cid, 32);
 	if (status != cgiFormSuccess)
 	{
-		fprintf(cgiOut, "get sname error!\n");
+		fprintf(cgiOut, "get cid error!\n");
 		return 1;
 	}
 
@@ -45,15 +45,7 @@ int cgiMain()
 	MYSQL *db;
 	char sql[128] = "\0";
 
-	if (sname[0] == '*')
-	{
-		sprintf(sql, "select sid,sname,sex,age,scid from information where statu=1");
-	}
-	else
-	{
-		sprintf(sql, "select sid,sname,sex,age,scid from information where sname = '%s'", sname);
-	}
-
+	sprintf(sql, "select * from course");
 
 	//初始化
 	db = mysql_init(NULL);
